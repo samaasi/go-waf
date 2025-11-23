ee658f17e74d48380a92705596126ad1da3a7c5b
Benson Samaasi <dev.bensonsamaasi@gmail.com>
2025-11-23 09:29:32 +0100

    feat(domain): add rule and rule engine interfaces for waf
    Add core interfaces for WAF rule evaluation system including Rule and RuleEngine

---
+++ internal/domain/rule.go
@@ -0,0 +1,16 @@
+package domain
+
+// Rule represents a single detection logic unit.
+type Rule interface {
+	ID() string
+	Name() string
+	Tags() []string
+	Evaluate(req *WafRequest) (bool, string)
+	Severity() Severity
+}
+
+// RuleEngine describes a component that can manage and execute a set of rules.
+type RuleEngine interface {
+	LoadRules() error
+	Evaluate(req *WafRequest) []*SecurityEvent
+}
